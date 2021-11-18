c5b8605398c6e9c7a23e699e22d24c51dbe67e6d
mazenmazosh <mazenmazosh@gmail.com>
2021-11-18 06:44:04 +0200

    third change

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:personal_expenses_app/models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({Key? key}) : super(key: key);
+  Chart(this.recentTransactions, {Key? key}) : super(key: key);
+
+  final List<Transaction> recentTransactions;
+
+  List<Map<String, dynamic>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      var totalSum = 0.0;
+
+      for (int i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].ammount!;
+        }
+      }
+
+      print(DateFormat.E(weekDay));
+      print(totalSum);
+      return {
+        'day': DateFormat.E(weekDay),
+        'ammount': totalSum,
+      };
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
     );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,74 +8,70 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-          height: 320,
-          child: transactions.isEmpty
-              ? Expanded(
-                  child: Column(
-                    children: [
-                      Text(
-                        'No transactions added yet !',
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                          height: 200,
-                          child: Image.asset(
-                            'assets/images/waitting.png',
-                            fit: BoxFit.cover,
-                          )),
-                    ],
+    return Container(
+        height: 320,
+        child: transactions.isEmpty
+            ? Column(
+                children: [
+                  Text(
+                    'No transactions added yet !',
+                    style: Theme.of(context).textTheme.headline6,
                   ),
-                )
-              : ListView.builder(
-                  itemBuilder: (context, index) {
-                    return Card(
-                        child: Row(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(20)),
-                              border:
-                                  Border.all(color: Colors.purple, width: 2.5)),
-                          margin: const EdgeInsets.all(20),
-                          child: Text(
-                            '\$ ${transactions[index].ammount!.toStringAsFixed(2)}',
-                            style: const TextStyle(
-                                color: Colors.purple,
-                                fontWeight: FontWeight.bold),
-                          ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                      height: 200,
+                      child: Image.asset(
+                        'assets/images/waitting.png',
+                        fit: BoxFit.cover,
+                      )),
+                ],
+              )
+            : ListView.builder(
+                itemBuilder: (context, index) {
+                  return Card(
+                      child: Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            border:
+                                Border.all(color: Colors.purple, width: 2.5)),
+                        margin: const EdgeInsets.all(20),
+                        child: Text(
+                          '\$ ${transactions[index].ammount!.toStringAsFixed(2)}',
+                          style: const TextStyle(
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold),
                         ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              transactions[index].title,
-                              style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20),
-                            ),
-                            Text(
-                              DateFormat('dd-MM-yyyy')
-                                  .format(transactions[index].date),
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                  color: Colors.grey),
-                            )
-                          ],
-                        )
-                      ],
-                    ));
-                  },
-                  itemCount: transactions.length,
-                )),
-    );
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            transactions[index].title,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20),
+                          ),
+                          Text(
+                            DateFormat('dd-MM-yyyy')
+                                .format(transactions[index].date),
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.grey),
+                          )
+                        ],
+                      )
+                    ],
+                  ));
+                },
+                itemCount: transactions.length,
+              ));
   }
 }
