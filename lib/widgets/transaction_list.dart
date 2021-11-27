f21ba37e17081a3c4889575c688e3ea16b04e863
mazenmazosh <mazenmazosh@gmail.com>
2021-11-27 12:22:20 +0200

    fifth change

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -31,45 +31,27 @@ class TransactionList extends StatelessWidget {
             : ListView.builder(
                 itemBuilder: (context, index) {
                   return Card(
-                      child: Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(20)),
-                            border:
-                                Border.all(color: Colors.purple, width: 2.5)),
-                        margin: const EdgeInsets.all(20),
-                        child: Text(
-                          '\$ ${transactions[index].amount.toStringAsFixed(2)}',
-                          style: const TextStyle(
-                              color: Colors.purple,
-                              fontWeight: FontWeight.bold),
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: FittedBox(
+                              child: Text('\$${transactions[index].amount}')),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            transactions[index].title,
-                            style: const TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20),
-                          ),
-                          Text(
-                            DateFormat('dd-MM-yyyy')
-                                .format(transactions[index].date),
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.grey),
-                          )
-                        ],
-                      )
-                    ],
-                  ));
+                      title: Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 17),
+                      ),
+                      subtitle: Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                      ),
+                    ),
+                  );
                 },
                 itemCount: transactions.length,
               ));
